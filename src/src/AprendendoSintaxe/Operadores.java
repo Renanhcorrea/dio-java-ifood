c92b07a9ea2e40f4887d8891f1bc86f983278be4
Renan <renanhenrikecorrea@hotmail.com>
2022-10-31 21:05:19 +0000

    bootcamp

---
+++ src/src/AprendendoSintaxe/Operadores.java
@@ -0,0 +1,32 @@
+package AprendendoSintaxe;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        /*
+        =   - atribuição
+        ==  - igual
+        !=  - diferente
+        <   - menor
+        >   - maior
+        <=  - menor igual
+        >=  - maior igual
+        */
+        // Atribuição
+        String nome = "Renan";
+        int idade = 30;
+
+        // Aritméticos
+
+        double soma = 10.5 + 15.7; // também utilizamos " + " para concatenar textos.
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15/3;
+        int modulo = 18 % 3;
+        double resultado = (20*5) + (20/3);
+
+        System.out.println("Linguagem " + "Java");
+    }
+}
---
+++ src/src/AprendendoSintaxe/TiposVariaveis.java
@@ -0,0 +1,22 @@
+package AprendendoSintaxe;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+
+        double salarioMinimo = 2500.33; // tipo "double", nome "salarioMinimo", com valor anexado.
+
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 210703333; // se começar com zero talvez tera que ser outro tipo.
+        long cpf = 98765432109L; // se começar com zero talvez tera que ser outro tipo e precisa colocar o L no final.
+        float pi = 3.14F; // precisa por o F no final.
+        double salario = 1275.33;
+
+        cep = 215987302; // podemos alterar o valor das variaveis.
+
+        final int ANO = 2022; // para a variavel nunca mudar.
+
+
+
+    }
+}
